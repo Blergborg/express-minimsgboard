39cf11d0f70f721c496307908b624cb9a1d35e73
Phil Prater <pprater92@gmail.com>
2022-06-20 12:47:18 -0700

    basic post functionality. added heroku Procfile.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amanda",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
+
+router.get('/new', (req, res, next) => {
+  res.render('form', {title: 'New Message'});
+})
+
+router.post('/new', (req, res, next) => {
+  const messageText = req.body.message;
+  const messageAuthor = req.body.author;
+  messages.push({text: messageText, user: messageAuthor, added: new Date()});
+  res.redirect('/')
+})
 
 module.exports = router;
